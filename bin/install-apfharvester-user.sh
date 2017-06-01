854ed81d00d0134c76ab01a2bdf07669163624c5
jhover <jhover@192.168.1.103>
2017-06-01 10:09:08 -0400

    Update.

--- bin/install-apfharvester-user.sh
+++ bin/install-apfharvester-user.sh
@@ -4,20 +4,30 @@
 #
 echo virtualenv harvester
 virtualenv harvester
+
 echo cd harvester
 cd harvester
+
 echo . bin/activate
 . bin/activate
+
 echo pip install pip --upgrade
 pip install pip --upgrade
+
 echo pip install python-daemon
 pip install python-daemon --upgrade
+
 echo pip install requests
 pip install requests --upgrade
+
 echo pip install git+git://github.com/PanDAWMS/panda-common.git@setuptools
 pip install git+git://github.com/PanDAWMS/panda-common.git@setuptools --upgrade
+
 #echo pip install git+git://github.com/PanDAWMS/panda-harvester.git
 #pip install git+git://github.com/PanDAWMS/panda-harvester.git --upgrade
+
+echo pip install git+git://github.com/PanDAWMS/autopyfactory.git --upgrade
+pip install git+git://github.com/PanDAWMS/autopyfactory.git --upgrade
 
 echo pip install git+git/github.com/bnl-sdcc/panda-harvester  --upgrade
 pip install git+git://github.com/bnl-sdcc/panda-harvester  --upgrade
@@ -25,19 +35,23 @@ sleep 2
 
 echo cp ~/git/autopyfactory-harvester/configs/jrh-panda_harvester.init etc/rc.d/init.d/panda_harvester
 cp ~/git/autopyfactory-harvester/configs/jrh-panda_harvester.init etc/rc.d/init.d/panda_harvester
+
 echo cp ~/git/autopyfactory-harvester/configs/jrh-panda_common.cfg etc/panda/panda_common.cfg
 cp ~/git/autopyfactory-harvester/configs/jrh-panda_common.cfg etc/panda/panda_common.cfg
+
 echo cp ~/git/autopyfactory-harvester/configs/jrh-panda_harvester etc/sysconfig/panda_harvester
 cp ~/git/autopyfactory-harvester/configs/jrh-panda_harvester etc/sysconfig/panda_harvester
+
 echo cp~/git/autopyfactory-harvester/configs/jrh-panda_harvester.cfg  etc/panda/panda_harvester.cfg
 cp ~/git/autopyfactory-harvester/configs/jrh-panda_harvester.cfg  etc/panda/panda_harvester.cfg
+
 echo cp ~/git/autopyfactory-harvester/configs/jrh-panda_queueconfig.json etc/panda/panda_queueconfig.json
 cp ~/git/autopyfactory-harvester/configs/jrh-panda_queueconfig.json etc/panda/panda_queueconfig.json
 
 chmod +x etc/rc.d/init.d/panda_harvester
 mkdir -p log  
 mkdir -p var/log/panda/
-mkdir -p var/log/harvester
+mkdir -p var/harvester
 mkdir -p var/run
 mkdir -p tmp
 
