6aabc73016d96e70d8712a42e597948ac86c5422
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-06-02 10:57:19 -0400

    Update.

--- bin/install-apfharvester-deps-root.sh
+++ bin/install-apfharvester-deps-root.sh
@@ -5,8 +5,8 @@
 echo yum -y install python-virtualenv
 yum -y install python-virtualenv
 
-echo yum -y install condor condor-python
-yum -y install condor condor-python
+#echo yum -y install condor condor-python
+#yum -y install condor condor-python
 
 echo yum -y install voms-clients-cpp ca-certificates
 yum -y install voms-clients-cpp ca-certificates
@@ -16,16 +16,16 @@ rpm -ivh  https://repo.grid.iu.edu/osg/3.3/osg-3.3-el7-release-latest.rpm
 
 yum -y install vo-client
 
-echo "DAEMON_LIST = COLLECTOR, MASTER, NEGOTIATOR, SCHEDD >> /etc/condor/config.d/00personal_condor.config"
-echo DAEMON_LIST = COLLECTOR, MASTER, NEGOTIATOR, SCHEDD >> /etc/condor/config.d/00personal_condor.config 
+#echo "DAEMON_LIST = COLLECTOR, MASTER, NEGOTIATOR, SCHEDD >> /etc/condor/config.d/00personal_condor.config"
+#echo DAEMON_LIST = COLLECTOR, MASTER, NEGOTIATOR, SCHEDD >> /etc/condor/config.d/00personal_condor.config 
 
-echo service start condor
-service start condor
+#echo service start condor
+#service start condor
 
-echo ps auxf | grep condor
-ps auxf | grep condor
+#echo ps auxf | grep condor
+#ps auxf | grep condor
 
-echo useradd harvester
-useradd harvester
+#echo useradd harvester
+#useradd harvester
 
-echo Now su -l harvester and run user install. 
+#echo Now su -l harvester and run user install. 
