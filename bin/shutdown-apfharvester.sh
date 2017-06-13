f4c2aeb9601142d11c76f4a3c94a0e884418ed3c
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-06-13 10:20:51 -0400

    Update.

---
+++ bin/shutdown-apfharvester.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo cd ~/harvester
+cd ~/harvester
+
+echo . ./bin/activate
+. ./bin/activate
+
+echo . condor.sh
+. condor.sh
+
+echo . etc/sysconfig/panda_harvester
+. etc/sysconfig/panda_harvester
+
+echo ./etc/rc.d/init.d/panda_harvester stop
+./etc/rc.d/init.d/panda_harvester stop
+
+echo condor_rm -all
+condor_rm -all
+
+echo sleep 10
+sleep 10
+
+echo condor_off -master
+condor_off -master
