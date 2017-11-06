22c15080819c6c1666b912b650a7c7f38eb0f3a8
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-11-06 14:09:01 -0500

    Update.

--- bin/install-apfharvester-user.sh
+++ bin/install-apfharvester-user.sh
@@ -98,7 +98,11 @@ cp ~/git/autopyfactory-harvester/configs/jrh-auth.conf etc/autopyfactory/auth.co
 echo cp ~/git/autopyfactory-harvester/libexec/wrapper* libexec/
 cp ~/git/autopyfactory-harvester/libexec/wrapper* libexec/
 
-# Houseousekeeping.
+echo cp ~/git/autopyfactory-harvester/shutdown-apfharvester.sh bin/
+cp ~/git/autopyfactory-harvester/shutdown-apfharvester.sh bin/
+
+
+# Housekeeping.
 chmod +x etc/rc.d/init.d/panda_harvester
 chmod +x libexec/wrapper*
 ln -s ~/harvester/etc/rc.d/init.d/panda_harvester ~/harvester/etc/init.d
---
+++ bin/uninstall-apfharvester.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Fast and dirty
+# APF Grid Harvester turn off, clean script. 
+#
+# John Hover <jhover@bnl.gov>
+#
