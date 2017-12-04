510e0cab009a91d4dbf616e0209b179c7a93aab0
jhover <jhover@starkhorn.rhic.bnl.gov>
2017-12-04 11:39:17 -0500

    Update.

--- bin/reinstall-apfharvester.sh
+++ bin/reinstall-apfharvester.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+
+set -xe
+
 ~/harvester/etc/rc.d/init.d/panda_harvester stop 
 killall condor_master 
 cd ~/ 
 rm -rf harvester /tmp/harvester.db 
 cd ~/git/autopyfactory-harvester 
 git pull 
-cd 
-install-apfharvester-user.sh
+./bin/install-apfharvester.sh
